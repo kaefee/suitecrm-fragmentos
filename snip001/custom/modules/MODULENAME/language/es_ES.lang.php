54c3863dda25119fefc8e84e56daf9721a6aecd6
kaefee <31226521+kaefee@users.noreply.github.com>
2018-02-01 17:24:49 -0500

    Create es_ES.lang.php

---
+++ snip001/custom/modules/MODULENAME/language/es_ES.lang.php
@@ -0,0 +1,11 @@
+<?php
+// adicionar las etiquetas correspondientes
+$mod_strings = array (
+//.....
+  'LBL_FIELD1' => 'Campo 1',
+  'LBL_FIELD1' => 'Campo 2',
+  'LBL_FIELD1' => 'Campo 3',
+  'LBL_FIELD1' => 'Campo 4',
+
+
+);
