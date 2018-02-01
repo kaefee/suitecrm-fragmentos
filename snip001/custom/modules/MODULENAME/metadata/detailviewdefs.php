5d716a05cde7987720dc32305d2447989464a561
kaefee <31226521+kaefee@users.noreply.github.com>
2018-02-01 17:22:18 -0500

    Create detailviewdefs.php

---
+++ snip001/custom/modules/MODULENAME/metadata/detailviewdefs.php
@@ -0,0 +1,35 @@
+//Modificaciones necesarias de acuerdo a los campos insertados
+// NUMX corresponde al numero de fila creada en el estudio para la visualizacion de los campos 1,2,3,etc
+
+
+ NUMX1 => 
+        array (
+          0 => 
+          array (
+            'name' => 'field1_c',
+            'label' => 'LBL_FIELD1',
+            'customCode' => '{$CUSTOM_FIELD1}',
+          ),
+          1 => 
+          array (
+            'name' => 'field2_c',
+            'label' => 'LBL_FIELD2',
+            'customCode' => '{$CUSTOM_FIELD2}',
+
+          ),
+        ),
+ NUMX2 => 
+        array (
+          0 => 
+          array (
+            'name' => 'field3_c',
+            'label' => 'LBL_FIELD3',
+            'customCode' => '{$CUSTOM_FIELD3}',
+          ),
+          1 => 
+          array (
+            'name' => 'field4_c',
+            'label' => 'LBL_FIELD4',
+            'customCode' => '{$CUSTOM_FIELD4}',
+          ),
+        ),
